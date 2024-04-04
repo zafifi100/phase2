e4ebe2d026e6d59b83440a438e0c64580b7dc702
Rago200 <rsenthilkuma@wisc.edu>
2024-04-04 12:46:29 -0500

    Turns out / can not be in the variable name.

--- forward_unit.v
+++ forward_unit.v
@@ -1,28 +1,28 @@
 module forwardunit (
-    input EX/MEM_Regwrite, MEM/WB_Regwrite;
-    input [3:0] EX/MEM_RegisterRd, ID/EX_RegisterRs,  ID/EX_RegisterRt, MEM/WB_RegisterRd, EX/MEM_RegisterRt; 
+    input EX_MEM_Regwrite, MEM_WB_Regwrite;
+    input [3:0] EX_MEM_RegisterRd, ID_EX_RegisterRs,  ID_EX_RegisterRt, MEM_WB_RegisterRd, EX_MEM_RegisterRt; 
     output [1:0] ForwardA;
     output [1:0] ForwardB;
     output ForwardMem;
 );
 
 //Wires
-wire EX/EX_S1, MEM/EX_S1;
-wire EX/EX_S2, MEM/EX_S2;
+wire EX_EX_S1, MEM_EX_S1;
+wire EX_EX_S2, MEM_EX_S2;
 
 /// EX-EX Forwarding
-assign EX/EX_S1 = (EX/MEM_Regwrite) && (EX/MEM_RegisterRd != 4'b0000) && (EX/MEM_RegisterRd == ID/EX_RegisterRs);
-assign EX/EX_S2 = (EX/MEM_Regwrite) && (EX/MEM_RegisterRd != 4'b0000) && (EX/MEM_RegisterRd == ID/EX_RegisterRt);
+assign EX_EX_S1 = (EX_MEM_Regwrite) && (EX_MEM_RegisterRd != 4'b0000) && (EX_MEM_RegisterRd == ID_EX_RegisterRs);
+assign EX_EX_S2 = (EX_MEM_Regwrite) && (EX_MEM_RegisterRd != 4'b0000) && (EX_MEM_RegisterRd == ID_EX_RegisterRt);
 
 /// MEM-EX Forwarding
-assign MEM/EX_S1 = (MEM/WB_Regwrite) && (EX/EX_S1 != 1'b1) && (MEM/WB_RegisterRd != 4'b0000) && (MEM/WB_RegisterRd == ID/EX_RegisterRs);
-assign MEM/EX_S2 = (MEM/WB_Regwrite) && (EX/EX_S2 != 1'b1) && (MEM/WB_RegisterRd != 4'b0000) && (MEM/WB_RegisterRd == ID/EX_RegisterRt);
+assign MEM_EX_S1 = (MEM_WB_Regwrite) && (EX_EX_S1 != 1'b1) && (MEM_WB_RegisterRd != 4'b0000) && (MEM_WB_RegisterRd == ID_EX_RegisterRs);
+assign MEM_EX_S2 = (MEM_WB_Regwrite) && (EX_EX_S2 != 1'b1) && (MEM_WB_RegisterRd != 4'b0000) && (MEM_WB_RegisterRd == ID_EX_RegisterRt);
 
 //Assigning ForwardA and B
-assign ForwardA = (EX/EX_S1) ? 2'b10 : (MEM/EX_S1) ? 2'b01 : 2'b00;
-assign ForwardB = (EX/EX_S2) ? 2'b10 : (MEM/EX_S2) ? 2'b01 : 2'b00;
+assign ForwardA = (EX_EX_S1) ? 2'b10 : (MEM_EX_S1) ? 2'b01 : 2'b00;
+assign ForwardB = (EX_oEX_S2) ? 2'b10 : (MEM_EX_S2) ? 2'b01 : 2'b00;
 
 /// MEM-MEM Forwarding
-assign ForwardMem = MEM/WB_Regwrite && (MEM/WB_RegisterRd != 4'b0000) && (MEM/WB_RegisterRd == EX/MEM_RegisterRt);
+assign ForwardMem = MEM_WB_Regwrite && (MEM_WB_RegisterRd != 4'b0000) && (MEM_WB_RegisterRd == EX_MEM_RegisterRt);
 
 endmodule
--- hazard_detection.v
+++ hazard_detection.v
@@ -1,9 +1,9 @@
 module hazarddetection (
-    input ID/EX_MemRead,  IF/ID_MemWrite;
-    input [3:0] ID/EX_RegisterRd, IF/ID_RegisterRs, IF/ID_RegisterRt;
+    input ID_EX_MemRead,  IF_ID_MemWrite;
+    input [3:0] ID_EX_RegisterRd, IF_ID_RegisterRs, IF_ID_RegisterRt;
     output enable_stall;
 );
 
-assign enable_stall = (ID/EX_MemRead & (ID/EX_RegisterRd != 4'b0000) & ((ID/EX_RegisterRd == IF/ID_RegisterRs) | ((ID/EX_RegisterRd == IF/ID_RegisterRt) & (~IF/ID_MemWrite))));
+assign enable_stall = (ID_EX_MemRead & (ID_EX_RegisterRd != 4'b0000) & ((ID_EX_RegisterRd == IF_ID_RegisterRs) | ((ID_EX_RegisterRd == IF_ID_RegisterRt) & (~IF_ID_MemWrite))));
 
 endmodule
