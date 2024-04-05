d11bbd7c8acd1113527b460fea0fe4833a238569
Rago200 <rsenthilkuma@wisc.edu>
2024-04-05 11:57:58 -0500

    Did Some logic for ID_EX_PipelineReg

---
+++ ID_EX_PipelineReg.v
@@ -0,0 +1,16 @@
+module  ID_EX_PipelineReg (
+    input clk, rst_n, ReadIn, WriteReg, PCS, MemtoReg, MemRead, MemWrite, B, BR, HLT;
+    input [1:0] AluSrc1, AluSrc2;
+    input [2:0] AluOp; 
+    input [15:0] ReadData1, ReadData2;
+);
+
+//Excute Stage Signals
+
+//Memory Stage Signals
+
+//Write Back Stage Signals
+
+//
+    
+endmodule
