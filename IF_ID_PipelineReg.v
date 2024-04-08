001261e82ea367dd6c09d16929cd256c23e79d27
zafifi100 <114939084+zafifi100@users.noreply.github.com>
2024-04-07 22:01:11 -0500

    updated flush logic

--- IF_ID_PipelineReg.v
+++ IF_ID_PipelineReg.v
@@ -1,10 +1,10 @@
 module IF_ID_PipelineReg (
-    input clk, rst, enable_stall,
+    input clk, rst, enable_stall, Flush
     input [15:0] pc, instr,
     output [15:0] pc_out, instr_out
 );
 
-dff_16 pc_ff(.q(pc_out), .d(pc), .clk(clk), .rst(rst), .wen(enable_stall));
-dff_16 instrr(.q(instr_out), .d(instr), .rst(rst), .wen(enable_stall));
+  dff_16 pc_ff(.q(pc_out), .d(pc), .clk(clk), .rst(rst), .wen(enable_stall));
+  dff_16 instrr(.q((!Flush) ? instr_out : 16'h4000), .d(instr), .rst(rst), .wen(enable_stall));
 
 endmodule
